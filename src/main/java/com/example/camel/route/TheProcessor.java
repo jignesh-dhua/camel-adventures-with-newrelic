f943339f10408e268c54c1342c7cbee7709a75ec
Jignesh Dhua <jignesh.dhua1985@gmail.com>
2021-05-25 12:09:36 +0100

    adding new relic trace api

--- src/main/java/com/example/camel/route/TheProcessor.java
+++ src/main/java/com/example/camel/route/TheProcessor.java
@@ -1,5 +1,7 @@
 package com.example.camel.route;
 
+import com.newrelic.api.agent.Trace;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TheProcessor implements Processor {
 
+    @Trace
     @Override
     public void process(Exchange exchange) throws Exception {
         System.out.println("Inside Processor");
